31400cba26b7eee8a411e84f0b0c6a919c435e90
richardsonclay <c.richardson917@gmail.com>
2012-01-02 21:02:42 -0600

    extra credit section

--- ex1.py
+++ ex1.py
@@ -6,3 +6,20 @@ print 'Yeay, printing!'
 print 'I\'d much rather you \'not\'.'
 print 'I "said" do not touch this.'
 
+# extra credit section
+print "Entering extra credit section"
+
+messages = [
+    "Hello World!",
+    "Hello Again",
+    "I like typing this.",
+    "This is fun.",
+    'Yeay, printing!',
+    'I\'d much rather you \'not\'.',
+    'I "said" do not touch this.',
+]
+
+from random import choice
+
+print choice(messages)
+
