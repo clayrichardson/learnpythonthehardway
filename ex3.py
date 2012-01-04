edbb5453f37f80f7477c30d7fdbb1cddc0f6cdb9
richardsonclay <c.richardson917@gmail.com>
2012-01-04 17:05:59 -0600

    added first extra credit exercise

--- ex3.py
+++ ex3.py
@@ -1,24 +1,42 @@
+# extra credit stuff mixed in, yum.
 print "I will now count my chickens:"
 
+# order of operations, cool beans.
+
+# divide 30 by 6 to get 5, then add 25 and 5 to get 30
 print "Hens", 25 + 30 / 6
+
+# multiply 25 and 3 to get 75, then 75 modulus 4 to get 3, subtract 3 from 100 to get 97
 print "Roosters", 100 - 25 * 3 % 4
 
 print "Now I will count the eggs:"
 
+# 1 divided by 4 is 0, since we're dealing with integer math.
+# 4 modulus 2 equals 0, leaving 3 + 2 + 1 - 5 + 0 - 0 + 6
+# add 3 and 2 to get 5, add 1 to get 6, subtract 5 to get 1
+# subtract and add 0 to get 1, add 6 to get 7
 print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
 
 print "Is it true that 3 + 2 < 5 - 7?"
 
+# add 3 and 2 to get 5
+# subtract 7 from 5 to get -2
+# 5 is not less than -2, so the comparison is false
 print 3 + 2 < 5 - 7
 
+# add 3 and 2 to get 5
 print "What is 3 + 2?", 3 + 2
+# subtract 7 from 5 to get -2
 print "What is 5 - 7?", 5 - 7
 
 print "Oh, that's why it's False."
 
 print "How about some more."
 
+# 5 is greater than -2
 print "Is it greater", 5 > -2
+# 5 is greater than or equal to -2
 print "Is it greater or equal?", 5 >= -2
+# 5 is not less than or equal to -2
 print "Is it less or equal?", 5 <= -2
 
